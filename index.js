ad8db2a96eac1ea7a2356633bc1a21dad4deb42c
qfei <jazzqi@gmail.com>
2016-09-02 14:16:58 +0800

    add generator for alpha

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 
 var assign = require('object-assign');
 
-hexo.config.index_generator = assign({
-  per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
-  order_by: '-date'
-}, hexo.config.index_generator);
+hexo.config.index_generator = assign({}, hexo.config.index_generator);
 
 hexo.extend.generator.register('index', require('./lib/generator'));
+hexo.extend.generator.register('ares', require('./lib/generator'));
+hexo.extend.generator.register('investor', require('./lib/generator'));
+hexo.extend.generator.register('company', require('./lib/generator'));
+hexo.extend.generator.register('about', require('./lib/generator'));
+hexo.extend.generator.register('recruitment', require('./lib/generator'));
